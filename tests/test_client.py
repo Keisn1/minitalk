121faff25c6b9c0de97b6b60d20f880d9448c554
Kay Freyer <kfreyer@c2r13s9.42wolfsburg.de>
2024-12-09 20:37:10 +0100

    try to use fixture in parametrized test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from functools import reduce
 import subprocess
-import signal
+import pytest
 
 
-def test_client():
+@pytest.fixture
+def pid_no_perm():
     ps_command = subprocess.Popen(
         ["ps", "-eo", "pid,user"],
         stdout=subprocess.PIPE,
@@ -18,13 +19,39 @@ def test_client():
             map(lambda x: x.strip().split(" "), ps_out.split("\n")[1:30]),
         )
     )
-
     pid_no_perm = list(filter(lambda x: x[1] == "root", ps_out))[0][0]
+    return pid_no_perm
+
+
+@pytest.fixture
+def pid_not_ex():
+    ps_command = subprocess.Popen(
+        ["ps", "-eo", "pid,user"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+    )
+    ps_out, _ = ps_command.communicate()
+    ps_out = list(
+        map(
+            lambda x: (int(x[0]), x[1]),
+            map(lambda x: x.strip().split(" "), ps_out.split("\n")[1:30]),
+        )
+    )
     pid_not_ex = [i + 1 for i, pid in enumerate(ps_out) if (i + 1) != pid[0]][0]
-    print(pid_not_ex)
+    return pid_not_ex
 
+
+@pytest.mark.parametrize(
+    "pid,want",
+    [
+        (pid_no_perm, "You don't have permission to send a message to that target\n"),
+        (pid_not_ex, "The target you specified does not exist\n"),
+    ],
+)
+def test_client(pid, want):
     client_p = subprocess.Popen(
-        ["./client", str(pid_no_perm), "my message"],
+        ["./client", str(pid), "my message"],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
@@ -33,24 +60,7 @@ def test_client():
 
     assert client_p.stdout is not None
     stdout_client = client_p.stdout.readline()
-
-    assert (
-        "You don't have permission to send a message to that target\n" == stdout_client
-    )
-
-    client_p = subprocess.Popen(
-        ["./client", str(pid_not_ex), "my message"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,
-    )
-    client_p.wait()
-
-    assert client_p.stdout is not None
-    stdout_client = client_p.stdout.readline()
-
-    assert "The target you specified does not exist\n" == stdout_client
-
+    assert stdout_client == want
     # srv_p = subprocess.Popen(
     #     "./server",
     #     stdout=subprocess.PIPE,
